b8426520aecd4656e6845df7ba649375558dfcb9
cd-shashank <shashank.jain@collegedekho.com>
2023-09-08 16:21:51 +0530

    Done

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from .models import Product, ProductCategory, CartItem
+from .models import Product, ProductTag, CartItem, Category
 
 
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'stock', 'price']
-    autocomplete_fields = ("categories",) # For Select Drop Down UI
+    autocomplete_fields = ("tags",) # For Select Drop Down UI
 
 
-class CategoryAdmin(admin.ModelAdmin):
+class ProductTagAdmin(admin.ModelAdmin):
     list_display = ['name']
     search_fields = ('name',) # We are using autocomplete field in ProductAdmin
 
@@ -17,6 +17,12 @@ class CartItemAdmin(admin.ModelAdmin):
     list_display = ['user', 'product', 'quantity']
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name', 'parent_category']
+    autocomplete_fields = ('parent_category', )
+    search_fields = ('category_name',)
+
 admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductCategory, CategoryAdmin)
+admin.site.register(ProductTag, ProductTagAdmin)
 admin.site.register(CartItem, CartItemAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -9,7 +9,15 @@ class CartManager(models.Manager):
     def get_cart_total(self):
         return 100
 
-class ProductCategory(models.Model):
+class Category(models.Model):
+    category_name = models.CharField(max_length=100)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
+
+    def __str__(self):
+        return self.category_name
+
+
+class ProductTag(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self): return f'{self.name} - {self.id}'
@@ -20,7 +28,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product_images/')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    categories = models.ManyToManyField(ProductCategory, blank=True)
+    tags = models.ManyToManyField(ProductTag, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     objects = ProductManager()
 
     def __str__(self): return f'{self.name}-{self.id}'
